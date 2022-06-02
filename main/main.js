43d804481e282c94ab8b69c7f6f367454579d64c
dpwls0329 <dpwls0329@kakao.com>
2022-06-02 12:35:41 +0900

    update

--- main/main.js
+++ main/main.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
-      var imgs;
-      var img_count;
-      var img_position = 1;
+  var imgs;
+  var img_count;
+  var img_position = 1;
 
-      imgs = $(".slide ul");
-      img_count = imgs.children().length;//이미지 갯수
+  imgs = $(".slide ul");
+  img_count = imgs.children().length;//이미지 갯수
 
-      $('#back').click(function () {
-        back();
+  $('#back').click(function () {
+    back();
+  });
+  $('#next').click(function () {
+    next();
+  });
+
+  function back() {
+    if(1<img_position){
+      imgs.animate({
+        left:'+=1180px'
       });
-      $('#next').click(function () {
-        next();
+      img_position--;
+    }
+  }
+  function next() {
+    if(img_count>img_position){
+      imgs.animate({
+        left:'-=1180px'
       });
-
-      function back() {
-        if(1<img_position){
-          imgs.animate({
-            left:'+=1180px'
-          });
-          img_position--;
-        }
-      }
-      function next() {
-        if(img_count>img_position){
-          imgs.animate({
-            left:'-=1180px'
-          });
-          img_position++;
-        }
-      }
-    });
+      img_position++;
+    }
+  }
+});
